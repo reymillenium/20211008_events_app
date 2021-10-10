ca526049ac39af4fa8e8c8c05448976b84dab978
Reinier Garcia Ramos <reymillenium@gmail.com>
2021-10-10 18:33:29 -0400

    added components/Layout/MainNavigationEvents/MainNavigationEvents.js

--- components/Layout/Layout.js
+++ components/Layout/Layout.js
@@ -1,11 +1,13 @@
-import MainNavigation from './MainNavigation/MainNavigation';
+// import MainNavigation from './MainNavigation/MainNavigation';
+import MainNavigationEvents from "./MainNavigationEvents/MainNavigationEvents";
 import styles from './Layout.module.css';
 import FooterClassic from "../FooterClassic/FooterClassic";
 import PageHead from "../ui/PageHead";
 
 function Layout(props) {
     const hideNavigation = props.hideNavigation || false;
-    const navigation = (hideNavigation ? null : <MainNavigation/>);
+    // const navigation = (hideNavigation ? null : <MainNavigation/>);
+    const navigation = (hideNavigation ? null : <MainNavigationEvents/>);
 
     return (
         <>
---
+++ components/Layout/MainNavigationEvents/MainNavigationEvents.js
@@ -0,0 +1,70 @@
+import styles from './MainNavigationEvents.module.css';
+import Link from "next/link";
+import generateRoutes from "../../../tools/generateRoutes";
+
+const MainNavigationEvents = () => {
+    const routes = generateRoutes();
+    const eventsRoutes = routes.events;
+    console.log(eventsRoutes);
+
+    const eventsLinks = (
+        <>
+            <li>
+                <Link href={eventsRoutes.indexPath}>
+                    <div>
+                        <i className={" far fa-calendar-alt faa-wrench faa-fast"}/>&nbsp;Events
+                        {/*New Event*/}
+                    </div>
+                </Link>
+            </li>
+
+            <li>
+                <Link href={eventsRoutes.featuredIndexPath}>
+                    <div>
+                        <i className={" far fa-calendar-check faa-wrench faa-fast"}/>&nbsp;Featured
+                        {/*New Event*/}
+                    </div>
+                </Link>
+            </li>
+
+            <li>
+                <Link href={eventsRoutes.newPath}>
+                    <div>
+                        <i className={" far fa-calendar-plus faa-wrench faa-fast"}/>&nbsp;New Event
+                        {/*New Event*/}
+                    </div>
+                </Link>
+            </li>
+        </>
+    );
+
+    return (
+        <header className={styles.header}>
+            <div className={styles.logo}>
+                <Link href={eventsRoutes.indexPath}>
+                    <>
+                        {/*<Image src="/event_started_checked_logo_290_x_290.png" alt="Vercel Logo" width={72} height={16} />*/}
+
+                        {/*<Image src="/event_started_checked_logo_290_x_290.png" alt="Vercel Logo" width={32} height={32}/>*/}
+                        {/*<i style={{verticalAlign: 'baseline'}} className={" far fa-calendar-plus faa-wrench faa-fast"}/>*/}
+                        <i style={{verticalAlign: 'middle', color: 'white', fontWeight: 'inherit', marginBottom: 6}} className={" far fa-calendar-alt faa-wrench faa-fast"}/>&nbsp;Events 2021
+                    </>
+                </Link>
+            </div>
+
+            <nav className={styles.navigation}>
+                <ul>
+                    {/*{eventsLinks}*/}
+                    <li>
+                        <Link href={eventsRoutes.indexPath}>Next Event</Link>
+                    </li>
+                    <li>
+                        <Link href={eventsRoutes.indexPath}>Next Event</Link>
+                    </li>
+                </ul>
+            </nav>
+        </header>
+    );
+};
+
+export default MainNavigationEvents;
