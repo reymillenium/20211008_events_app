98ca40f25e2faff9d9e1cc63850c0ab5d79f6c4b
Reinier Garcia Ramos <reymillenium@gmail.com>
2021-10-11 23:05:56 -0400

    Added a dynammic title gemerator

--- pages/events/[...slug].js
+++ pages/events/[...slug].js
@@ -4,6 +4,7 @@ import {getFilteredEvents} from "../../dummy-data";
 import EventsSearch from "../../components/events/EventsSearch/EventsSearch";
 import EventItemsList from "../../components/events/EventItemsList/EventItemsList";
 import EventContent from "../../components/events/EventDetail/EventContent";
+import {DUMMY_MONTHS_DATA} from "../../dummy-data";
 
 const FilteredEventsPage = () => {
     const router = useRouter();
@@ -15,8 +16,8 @@ const FilteredEventsPage = () => {
     }
 
     const slug = query.slug || [];
-    const [year, month, isFeatured] = slug;
-    const filteredEvents = getFilteredEvents({year: year, month: month});
+    const [yearStr, monthStr, isFeaturedStr] = slug;
+    const filteredEvents = getFilteredEvents({year: yearStr, month: monthStr});
 
     let filteredEventsContent;
     if (filteredEvents.length === 0) {
@@ -29,10 +30,28 @@ const FilteredEventsPage = () => {
         filteredEventsContent = <EventItemsList events={filteredEvents}/>;
     }
 
+    let titleLabel = `List of `;
+    if (yearStr === 'All' && monthStr === 'All' && isFeaturedStr === 'false') {
+        titleLabel += `all the Events`;
+    } else if (yearStr === 'All' && monthStr === 'All' && isFeaturedStr === 'true') {
+        titleLabel += `all the Featured Events`;
+    } else {
+        titleLabel = (isFeaturedStr.toLowerCase() === 'true') ? `Featured Events` : `Events`;
+        const monthLabel = (monthStr !== 'All') ? DUMMY_MONTHS_DATA.find(monthData => monthData.value === parseInt(monthStr)).label : '';
+
+        if (yearStr === 'All' && monthStr !== 'All') {
+            titleLabel += ` of ${monthLabel}`;
+        } else if (yearStr !== 'All' && monthStr === 'All') {
+            titleLabel += ` of the year ${yearStr}`;
+        } else if (yearStr !== 'All' && monthStr !== 'All') {
+            titleLabel += ` of ${monthLabel}, ${yearStr}`;
+        }
+    }
+
     return (
         <div>
-            <h1>Events Slug Page</h1>
-            <EventsSearch initialYear={year} initialMonth={month} initialIsFeatured={isFeatured}/>
+            <h1>{titleLabel}</h1>
+            <EventsSearch initialYear={yearStr} initialMonth={monthStr} initialIsFeatured={isFeaturedStr}/>
             {filteredEventsContent}
         </div>
     );
