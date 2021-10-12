08038e402e3ae9001496e6ef5524d1912c21431f
Reinier Garcia Ramos <reymillenium@gmail.com>
2021-10-12 01:25:13 -0400

    Perfected the dates validation at pages/events/[...slug].js

--- pages/events/[...slug].js
+++ pages/events/[...slug].js
@@ -17,34 +17,48 @@ const FilteredEventsPage = () => {
 
     const slug = query.slug || [];
     const [yearStr, monthStr, isFeaturedStr] = slug;
-    const filteredEvents = getFilteredEvents({year: yearStr, month: monthStr});
 
     let filteredEventsContent;
-    if (filteredEvents.length === 0) {
+    let titleLabel = ``;
+    const isValidYear = (yearStr === 'All' || (!isNaN(+yearStr)));
+    const isValidMonth = (monthStr === 'All' || monthStr.match(/^\d+$/));
+
+    // console.log('isValidYear: ', isValidYear);
+    // console.log('isValidMonth: ', isValidMonth);
+    if (!isValidYear || !isValidMonth) {
         filteredEventsContent = (
             <EventContent>
-                <h1>No events were found</h1>
+                <p>Invalid filter. Please adjust your values</p>
             </EventContent>
         );
     } else {
-        filteredEventsContent = <EventItemsList events={filteredEvents}/>;
-    }
+        const filteredEvents = getFilteredEvents({year: yearStr, month: monthStr});
+        titleLabel = `List of `;
+        if (yearStr === 'All' && monthStr === 'All' && isFeaturedStr === 'false') {
+            titleLabel += `all the Events`;
+        } else if (yearStr === 'All' && monthStr === 'All' && isFeaturedStr === 'true') {
+            titleLabel += `all the Featured Events`;
+        } else {
+            titleLabel = (isFeaturedStr.toLowerCase() === 'true') ? `Featured Events` : `Events`;
+            const monthLabel = (monthStr !== 'All') ? DUMMY_MONTHS_DATA.find(monthData => monthData.value === parseInt(monthStr)).label : '';
 
-    let titleLabel = `List of `;
-    if (yearStr === 'All' && monthStr === 'All' && isFeaturedStr === 'false') {
-        titleLabel += `all the Events`;
-    } else if (yearStr === 'All' && monthStr === 'All' && isFeaturedStr === 'true') {
-        titleLabel += `all the Featured Events`;
-    } else {
-        titleLabel = (isFeaturedStr.toLowerCase() === 'true') ? `Featured Events` : `Events`;
-        const monthLabel = (monthStr !== 'All') ? DUMMY_MONTHS_DATA.find(monthData => monthData.value === parseInt(monthStr)).label : '';
+            if (yearStr === 'All' && monthStr !== 'All') {
+                titleLabel += ` of ${monthLabel}`;
+            } else if (yearStr !== 'All' && monthStr === 'All') {
+                titleLabel += ` of the year ${yearStr}`;
+            } else if (yearStr !== 'All' && monthStr !== 'All') {
+                titleLabel += ` of ${monthLabel}, ${yearStr}`;
+            }
+        }
 
-        if (yearStr === 'All' && monthStr !== 'All') {
-            titleLabel += ` of ${monthLabel}`;
-        } else if (yearStr !== 'All' && monthStr === 'All') {
-            titleLabel += ` of the year ${yearStr}`;
-        } else if (yearStr !== 'All' && monthStr !== 'All') {
-            titleLabel += ` of ${monthLabel}, ${yearStr}`;
+        if (filteredEvents.length === 0) {
+            filteredEventsContent = (
+                <EventContent>
+                    <h1>No events were found</h1>
+                </EventContent>
+            );
+        } else {
+            filteredEventsContent = <EventItemsList events={filteredEvents}/>;
         }
     }
 
