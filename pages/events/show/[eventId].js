7b3871767a03ad4884aa43f02472b1c62f82583c
Reinier Garcia Ramos <reymillenium@gmail.com>
2021-10-13 17:54:19 -0400

    Added some comments

--- pages/events/show/[eventId].js
+++ pages/events/show/[eventId].js
@@ -55,9 +55,9 @@ export async function getStaticPaths() {
         // If it fails pre-generating the dynamic age: With false (this is all the supported parameters) then it shows a 404 error -> 404 page
         //... and with true it then tries to generate a page for that meetupId, dynamically on the server for the incoming request,... How? by executing again the getStaticProps function?
         // Basically if he have hundred or thousands of dynamic pages, then we don't want to pre-generate all of them, but maybe just our most popular pages and then generate some of them and the rest gets generated dynamically on the server for the incoming request (whatever that means!)
-        // False -> these are all the meetupIds, so NO fallback. Just show a 404 error page
-        // True -> there might be other meetupIds, so YES,... perform a fallback
-        // blocking -> Similar to true, but the user won't see anything until the page was pre-generated and the finished page will be served
+        // False -> these are all the meetupIds, so NO fallback. Just show a 404 error page is the request has parameters not included in 'paths'
+        // True -> there might be other meetupIds, so YES,... perform a fallback. Next.js will pre-generate the page just in time, but Next.js won't wait until is pre-generated, so it needs a fallback block in the page component function
+        // blocking -> Similar to true, but the user won't see anything until the page was pre-generated in the server and then... the finished page will be served. Next.js will wait until is pre-generated just in time. No need to add a fallback block in the page component function (if (!product) {...)
         // However, in a production server it always shows a 404 error?:
         fallback: false, // With true or with 'blocking': It will generate that page on demand and thereafter cache it. It will pre-generate it when needed
         // true vs blocking: With true it will immediately return an empty page and then pull down the generated content once that's done, so we need to handle that case that the page doesn't have the data yet
