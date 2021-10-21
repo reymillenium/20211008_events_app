18f2c0ac98a876b03463f7083e7217173c975394
Reinier Garcia Ramos <reymillenium@gmail.com>
2021-10-20 21:08:57 -0400

    Added the pages/_document.js in porder to customize the general html document

---
+++ pages/_document.js
@@ -0,0 +1,32 @@
+import Document, {Html, Head, Main, NextScript} from "next/document";
+
+class MyDocument extends Document {
+    // Default structure
+    // render() {
+    //     return (
+    //         <Html>
+    //             <Head/>
+    //             <body>
+    //             <Main/>
+    //             <NextScript/>
+    //             </body>
+    //         </Html>
+    //     );
+    // }
+    // Default structure
+    render() {
+        return (
+            <Html lang={'en'}>
+                <Head/>
+                <body>
+                {/* Allows to add html content outside of our application component tree (using react portals) */}
+                <div id={'overlays'}/>
+                <Main/>
+                <NextScript/>
+                </body>
+            </Html>
+        );
+    }
+}
+
+export default MyDocument;
