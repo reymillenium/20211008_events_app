01e6a711811b0f799481fca4cc5aa1fb04d87256
Reinier Garcia Ramos <reymillenium@gmail.com>
2021-10-20 20:17:51 -0400

    Added some extra meta fields at components/ui/PageHead/index.js

--- components/ui/PageHead/index.js
+++ components/ui/PageHead/index.js
@@ -4,13 +4,19 @@ const PageHead = (props) => {
     const headInfo = props.headInfo || {};
 
     return (
-        <>
-            <Head>
-                <title>{headInfo.title}</title>
-                <meta name={"description"} content={`${headInfo.description}`}/>
-                <link rel="shortcut icon" href="/event_started_checked_logo_wine_290_x_290.png"/>
-            </Head>
-        </>
+        <Head>
+            <title>{headInfo.title}</title>
+
+            <meta property="og:type" content="website"/>
+            <meta name="og:title" property="og:title" content={headInfo.title}/>
+            <meta name={"description"} content={`${headInfo.description}`}/>
+            <meta name={'viewport'} content={'initial-scale=1.0, with=device-width'}/>
+            <meta property="og:site_name" content="Proper Noun"/>
+            <meta charSet="utf-8"/>
+
+            <link rel="shortcut icon" type={'image/png'} sizes={"16x16"} href="/event_started_checked_logo_wine_290_x_290.png"/>
+            <link rel="apple-touch-icon" href="/event_started_checked_logo_wine_290_x_290.png"/>
+        </Head>
     );
 };
 
